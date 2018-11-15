4dfeca797a33d05dc65894f67656bdd6f742bdf9
Reed Perry <reedperry@users.noreply.github.com>
2018-11-14 23:21:52 -0700

    Don't reflect messages from the server back to the client that just sent them, but broadcast to all other clients.

--- src/server.js
+++ src/server.js
@@ -1,20 +1,32 @@
-const WebSocket = require("ws")
+const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 })
+const wss = new WebSocket.Server({ port: 8080 }, () => {
+  console.log('Server started.');
+});
 
 wss.broadcast = function broadcast(data) {
+  console.log('There are ', wss.clients.size, 'clients');
+
   wss.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(data)
+      client.send(data);
     }
-  })
-}
+  });
+};
 
-wss.on("connection", ws => {
-  ws.on("message", msg => {
-    console.log("[server] received: %s", msg)
-    wss.broadcast(msg)
-  })
+wss.on('connection', ws => {
+  ws.on('message', msg => {
+    console.log('[server] received: %s', msg);
+    console.log('[server] from client ', ws);
 
-  ws.send("Welcome!")
-})
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msg);
+      }
+    });
+
+    // wss.broadcast(msg);
+  });
+
+  ws.send('Welcome to react-chat!');
+});
