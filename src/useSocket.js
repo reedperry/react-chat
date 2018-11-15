1d5997e2d453dbaafba201c6aa0aafd5824d50cb
Reed Perry <reedperry@users.noreply.github.com>
2018-11-14 23:19:44 -0700

    Get received messages storing properly and split up main components a bit. Not yet separating sent messages.

---
+++ src/useSocket.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+export default function useSocket(socket) {
+  const [messages, setMessages] = useState([]);
+
+  function send(text) {
+    if (!text) {
+      return;
+    }
+    socket.send(text);
+  }
+
+  useEffect(() => {
+    const onMessage = event => {
+      setMessages([...messages, { content: event.data, id: event.timeStamp }]);
+    };
+
+    function onOpen(event) {
+    }
+
+    socket.addEventListener('open', onOpen);
+    socket.addEventListener('message', onMessage);
+    return () => {
+      socket.removeEventListener('open', onOpen);
+      socket.removeEventListener('message', onMessage);
+    };
+  });
+
+  return { messages, send };
+}
