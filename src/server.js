bbb0ff840479b3f3acde63d8e8d6efcad151578a
Reed Perry <reedperry@users.noreply.github.com>
2018-11-17 14:28:47 -0700

    server: Start requiring a token param for connection, and assigning unique IDs to clients

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 const WebSocket = require('ws');
+const url = require('url');
+
+const validTokens = ['123a', '456b', '789c'];
 
 const wss = new WebSocket.Server({ port: 8080 }, () => {
   console.log('Server started.');
@@ -12,19 +15,39 @@ wss.broadcast = function broadcast(data) {
   });
 };
 
-wss.on('connection', ws => {
-  console.log('There are ', wss.clients.size, 'clients');
+wss.on('connection', (ws, req) => {
+  const {
+    query: { token }
+  } = url.parse(req.url, true);
+
+  if (!token || !validTokens.includes(token)) {
+    console.log('Refusing connection with invalid token.');
+    ws.close();
+    return;
+  }
+
+  if (Array.from(wss.clients).some(c => c.id === token)) {
+    console.log('Refusing new connection from client: token ' + token + ' is already connected.');
+    ws.close();
+    return;
+  }
+
+  ws.id = token;
+  ws.connectionTime = new Date().getTime();
+
+  console.log('New connection:', {
+    id: ws.id,
+    connectionTime: ws.connectionTime
+  });
 
   ws.on('message', msg => {
-    console.log('[server] received: %s', msg);
+    console.log('[server] received: %s', msg, 'from client', ws.id);
 
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(msg);
       }
     });
-
-    // wss.broadcast(msg);
   });
 
   ws.send('Welcome to react-chat!');
